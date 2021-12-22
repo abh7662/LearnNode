837b7e9c64dba2b22600c57f237b500f1834fe0e
Abhishek Kumar <abh7662@gmail.com>
2021-12-22 13:27:42 +0530

    added commands in readme

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # LearnNode
 LearnNode
+
+credintial manger in windows
+git auth -- will give other commands
+git auth login
+git auth logout
+
+
+npm init -y
--- app.js
+++ app.js
@@ -1,12 +1,41 @@
 const chalk = require('chalk')
+const yargs = require('yargs')
 const getNotes = require('./notes.js')
 
-const command = process.argv[2]
 
-console.log(process.argv)
 
-if (command === 'add') {
-    console.log('Adding note!')
-} else if (command === 'remove') {
-    console.log('Removing note!')
-}
+yargs.command({
+    command: 'add',
+    describe: 'adding values',
+    handler: function () {
+        console.log('adding a note')
+    }
+})
+
+
+yargs.command({
+    command: 'remove',
+    describe: 'removing values',
+    handler: function () {
+        console.log('removing a note')
+    }
+})
+
+
+yargs.command({
+    command: 'list',
+    describe: 'lising values',
+    handler: function () {
+        console.log('showing list of a note')
+    }
+})
+
+
+yargs.command({
+    command: 'read',
+    describe: 'reading values',
+    handler: function () {
+        console.log('reading a note')
+    }
+})
+console.log(yargs.argv)
